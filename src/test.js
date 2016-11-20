02f43cbde353afb478f6af53b1e3e2e04c827749
heshequ <he_it@qq.com>
2016-11-20 19:34:20 +0800

    '重新整理'

--- src/test.js
+++ src/test.js
@@ -1,6 +1,28 @@
-const {MYSQL} = require('./config')
+const mysql = require('mysql')
 const main = require('./main')
 
+/**
+ * mysql 数据库设置
+ */
+const MYSQL_DATABASE = {
+  TOKEN: {
+    host: 'localhost',
+    user: 'root',
+    password: 'Ac591234',
+    database: 'he_wetoken'
+  }
+}
+
+/**
+ * mysql 连接池对象
+ */
+const MYSQL = {
+  TOKEN: mysql.createPool(MYSQL_DATABASE.TOKEN)
+}
+
+/**
+ * 模拟koa
+ */
 async function fun() {
   let token = await main.getToken(MYSQL.TOKEN, 'wx6c6398aa27bba6c6', '1cff35124bd1875e17fc840682025077')
   let ticket = await main.getTicket(MYSQL.TOKEN, ticket)
