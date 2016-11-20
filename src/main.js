945086f76660bcb136cfda7d6d1a77e5a64883b8
heshequ <he_it@qq.com>
2016-11-20 20:27:26 +0800

    '添加getSign与注释'

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 const _ = require('lodash')
 const mysql = require('he-mysql')
 const request = require('request')
+const jssha = require('jssha')
 
 /**
  * 配置项，token和ticket可以使用的秒数
  */
-const seconds = 5400
+const seconds = 3600
 
 /**
  * 获取accesstoken的包装方法
+ * @param Object db 数据库连接池对象
+ * @param String appid 微信公众号appid
+ * @param String secret 微信公众号的appsecret
  */
 var getToken = function (db, appid, secret) {
   return new Promise(function(resolve, reject) {
@@ -25,10 +29,12 @@ var getToken = function (db, appid, secret) {
 
 /**
  * 获取jsapiticket的包装方法
+ * @param Object db 数据库连接池对象
+ * @param String token 已经取得的access_token字符串
  */
-var getTicket = function (token) {
+var getTicket = function (db, token) {
   return new Promise(function(resolve, reject) {
-    getJSApiTicket(token).then(
+    getJSApiTicket(db, token).then(
       function (ticket) {
         resolve(ticket)
       },
@@ -40,6 +46,28 @@ var getTicket = function (token) {
 }
 
 /**
+ * 获取签名
+ * @param String ticket 已经取得的ticket对象
+ * @param String url url字符串
+ * @param String nonce 随机字符串
+ * @param String timestamp 字符串类型的时间戳，注意，必须是字符串
+ */
+var getSign = function (ticket, url, nonce, timestamp) {
+  var ret = {
+    ticket: ticket,
+    nonceStr: nonce,
+    timestamp: timestamp,
+    url: url
+  }
+  var string = raw(ret)
+  var jsSHA = require('jssha')
+  var shaObj = new jsSHA('SHA-1', 'TEXT')  //new jsSHA(string, 'TEXT');
+  shaObj.update(string)
+  ret.signature = shaObj.getHash("HEX")  //shaObj.getHash('SHA-1', 'HEX');
+  return ret
+}
+
+/**
  * 取10位时间戳
  */
 var getTime = function () {
@@ -48,6 +76,7 @@ var getTime = function () {
 
 /**
  * request promise
+ * @param String url 需要访问的url
  */
 var http = function (url) {
   return new Promise(function(resolve, reject) {
@@ -58,6 +87,25 @@ var http = function (url) {
       resolve(body)
     })
   })
+}
+
+/**
+ * 制作签名前，整理args对象
+ * @param Object args 需要整理的args对象
+ */
+var raw = function (args) {
+  let keys = Object.keys(args)
+  keys = keys.sort()
+  let newArgs = {}
+  keys.forEach(function (key) {
+    newArgs[key.toLowerCase()] = args[key]
+  })
+  var string = ''
+  for (var k in newArgs) {
+    string += '&' + k + '=' + newArgs[k]
+  }
+  string = string.substr(1)
+  return string
 }
 
 /**
@@ -72,7 +120,7 @@ var getAccessToken = async function (db, appid, secret) {
   let data = await mysql.query(db, ['select * from `token` where id=1'], null)
 
   // 根据是否存在有效缓存进行操作
-  if (time - seconds > data[0][0].time) {
+  if ((data[0][0].time + seconds) <= time ) {
     try {
       let body = await http('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + secret)
       body = JSON.parse(body)
@@ -88,7 +136,8 @@ var getAccessToken = async function (db, appid, secret) {
 
 /**
  * 取api_ticket
- * @param String accesstoken 连接mysql数据库时的配置项
+ * @param Object db 数据库连接对象
+ * @param String token 已经取回的token
  */
 var getJSApiTicket = async function (db, token) {
   let time = getTime()
@@ -114,3 +163,4 @@ var getJSApiTicket = async function (db, token) {
  */
 exports.getToken = getToken
 exports.getTicket = getTicket
+exports.getSign = getSign
--- src/test.js
+++ src/test.js
@@ -25,9 +25,11 @@ const MYSQL = {
  */
 async function fun() {
   let token = await main.getToken(MYSQL.TOKEN, 'wx6c6398aa27bba6c6', '1cff35124bd1875e17fc840682025077')
-  let ticket = await main.getTicket(MYSQL.TOKEN, ticket)
+  let ticket = await main.getTicket(MYSQL.TOKEN, token)
+  let sign = main.getSign(ticket, 'http://www.baidu.com', '随机字符串', '1479636741')
   console.log(token)
   console.log(ticket)
+  console.log(sign)
 }
 
 fun()
